17bdef5dd99357b0899155aec072b55efcacbcca
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-17 00:23:50 +0530

    add a way to find best fir unit

--- src/runtime/composables/useRelativeTimeFormatter.ts
+++ src/runtime/composables/useRelativeTimeFormatter.ts
@@ -1,26 +1,52 @@
 const useTimeFormatter = ( date: Date, relativeDate: Date, unit : "best fit" | "year" | "quarter" | "month" | "week" | "day" | "hour" | "minute" | "second") => {
     const timeDifference = computed(()=>date.getTime()-relativeDate.getTime())
 
-    const getValuesForTimeDifference = (timeDifference: number, unit :"best fit" | "year" | "quarter" | "month" | "week" | "day" | "hour" | "minute" | "second") => {
+    const getValuesForTimeDifference = (timeDifference: number, unit :"year" | "quarter" | "month" | "week" | "day" | "hour" | "minute" | "second") => {
         switch (unit) {
             case "year":
-                return timeDifference / (1000 * 60 * 60 * 24 * 365);
+                return timeDifference / (1000 * 60 * 60 * 24 * 365)
             case "quarter":
-                return timeDifference / (1000 * 60 * 60 * 24 * 91.25);
+                return timeDifference / (1000 * 60 * 60 * 24 * 91.25)
             case "month":
-                return timeDifference / (1000 * 60 * 60 * 24 * 30.44);
+                return timeDifference / (1000 * 60 * 60 * 24 * 30.44)
             case "week":
-                return timeDifference / (1000 * 60 * 60 * 24 * 7);
+                return timeDifference / (1000 * 60 * 60 * 24 * 7)
             case "day":
-                return timeDifference / (1000 * 60 * 60 * 24);
+                return timeDifference / (1000 * 60 * 60 * 24)
             case "hour":
-                return timeDifference / (1000 * 60 * 60);
+                return timeDifference / (1000 * 60 * 60)
             case "minute":
-                return timeDifference / (1000 * 60);
+                return timeDifference / (1000 * 60)
             case "second":
-                return timeDifference / 1000;
+                return timeDifference / 1000
             default:
-                return null;
+                return null
+        }
+    }
+
+    const findBestFitUnit = (timeDifference:number) : "year" | "quarter" | "month" | "week" | "day" | "hour" | "minute" | "second"  => {
+        const seconds = timeDifference / 1000
+        const minutes = seconds / 60
+        const hours = minutes / 60
+        const days = hours / 24
+        const weeks = days / 7
+        const months = days / 30.44
+        const years = days / 365
+
+        if (years >= 1) {
+            return "year" 
+        } else if (months >= 1) {
+            return "month" 
+        } else if (weeks >= 1) {
+            return "week" 
+        } else if (days >= 1) {
+            return "day" 
+        } else if (hours >= 1) {
+            return "hour" 
+        } else if (minutes >= 1) {
+            return "minute"
+        } else {
+            return "second"
         }
     }
     
